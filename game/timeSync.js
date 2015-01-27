5dd7d9e1dd9d080e2723c4473e9253bf91c2d2d5
Michael Goldfine <michaelrgoldfine@gmail.com>
2015-01-26 23:01:23 -0500

    Latency compensation updated to sample 150 times and use median

--- game/timeSync.js
+++ game/timeSync.js
@@ -1,15 +1,32 @@
 var PUBNUB_TIME_URL = 'http://pubsub.pubnub.com/time/0';
 
 timeSync = {
-    init: function(initComplete) {
+    init: function(numTimes, wait) {
+        var _this = this;
+        var interval = setInterval(function() {
+            _this.sampleDelay();
+            if (_this.aheadBySamples.length >= numTimes) {
+                clearInterval(interval);
+            }
+        }, wait);
+    },
+
+    sampleDelay: function() {
         var _this = this;
         var start = this.localTime();
         $.get(PUBNUB_TIME_URL, function(data) {
             var localTime = _this.localTime();
             var delay = localTime - start;
-            console.log("TIME DATA RECIEVED: ", data[0]);
-            _this.aheadBy = localTime - (data[0]/10000 + delay);
+            var aheadBy = localTime - (data[0]/10000 + delay);
+            _this.logSample(aheadBy);
         }, 'json');
+    },
+
+    logSample: function(delay) {
+        console.log("LOG SAMPLE: ", delay, " TO: ", this.aheadBySamples);
+        this.aheadBySamples.push(delay);
+        this.aheadBy = calcMedian(this.aheadBySamples);
+        console.log("AHEAD BY: ", this.aheadBy);
     },
 
     now: function() {
@@ -20,7 +37,50 @@ timeSync = {
 
     localTime: function() {
         return new Date().getTime();
-    }
+    },
 };
 
-timeSync.init();
+function calcMedian(values) {
+    values.sort( function(a,b) {return a - b;} );
+    var half = Math.floor(values.length/2);
+ 
+    if(values.length % 2)
+        return values[half];
+    else
+        return (values[half-1] + values[half]) / 2.0;
+}
+
+function calcTrimmedMean(array, p) {
+    var outliersRemoved = removeOutliers(array, p);
+    console.log("OUTLIERS REMOVED: ", outliersRemoved);
+    return calcAverage(outliersRemoved);
+}
+
+function removeOutliers(array, p) {
+    var len = array.length;
+    var newArray = [];
+    array.sort();
+    var numToRemove = roundToEven(len*p);
+    if (numToRemove >= len) {
+        numToRemove = len - 1;
+    }
+    var leftIndex = Math.ceil(numToRemove / 2);
+    var rightIndex = Math.floor(len - numToRemove / 2);
+    return array.slice(leftIndex, rightIndex);
+}
+
+function calcAverage(array) {
+    var total = 0;
+    for (var i = array.length - 1; i >= 0; i--) {
+        total += array[i];
+    }
+    return total / array.length;
+}
+
+function roundToEven(num) {
+    return 2 * Math.round(num / 2);
+}
+
+timeSync.aheadBySamples = [];
+
+timeSync.init(150, 50);
